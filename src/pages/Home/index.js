df93b7a02f56a607277603a09c09f975cc63ef79
talitamarin <talita.smarin@outlook.com>
2023-10-02 13:27:53 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import { Header } from '../../components/Header'
+import background from '../../assets/background.png'
+import './styles.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background app'/>
+        <div className='info'>
+          <div>
+            <input name='usuario' placeholder='@username' />
+            <button>Buscar</button>
+        </div>
+      </div>
+    </div>
+</div>
+  );
+}
+
+export default App;
